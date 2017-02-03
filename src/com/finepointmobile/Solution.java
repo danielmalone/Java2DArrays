16b44c435f0379987a48f036a9a27fe12406abc1
Daniel Malone <danieljmalone@gmail.com>
2017-02-02 20:35:17 -0600

    initial commit

---
+++ src/com/finepointmobile/Solution.java
@@ -0,0 +1,16 @@
+package com.finepointmobile;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+    }
+}
